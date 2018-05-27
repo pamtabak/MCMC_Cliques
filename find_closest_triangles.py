2bb2a043f5c73a5a570b2b95d44337502e5b1b52
Pamela Tabak <pamelatabak@poli.ufrj.br>
2018-05-27 20:37:38 -0300

    optimizing the way we find our closest triangles

--- find_closest_triangles.py
+++ find_closest_triangles.py
@@ -13,24 +13,24 @@ def find_closest_triangles (g, vertex_dict, pivot, clique):
 
 	while (len(queue) != 0 and len(triangles) == 0):
 		w = queue.pop(0) #BFS
-		for e in g.edges(): #edge u, v
-			u = e.source()
-			v = e.target()
-			if g.edge(v, vertex_dict[w]) != None and g.edge(u, vertex_dict[w]) != None:
-				t = []
-				t.append(g.vp.labels[v])
-				t.append(w)
-				t.append(g.vp.labels[u])
-				t.sort()
-				if (sublist(t, clique)):
-					continue
-				if (t not in triangles):
-					triangles.append(t)
 		neighbors = get_neighbors(w, g, vertex_dict)
-		for n in neighbors:
-			if (n not in marked_dict):
-				marked_dict[n] = True
-				queue.append(n)
+		for u in range(0, len(neighbors)):
+			n1 = neighbors[u]
+			if (n1 not in marked_dict):
+				marked_dict[n1] = True
+				queue.append(n1)
+			for v in range(u + 1, len(neighbors)):
+				n2 = neighbors[v]
+				if (g.edge(vertex_dict[n1], vertex_dict[n2]) != None):
+					t = []
+					t.append(g.vp.labels[vertex_dict[n2]])
+					t.append(w)
+					t.append(g.vp.labels[vertex_dict[n1]])
+					t.sort()
+					if (sublist(t, clique)):
+						continue
+					if (t not in triangles):
+						triangles.append(t)
 	return triangles
 
 def sublist(list1, list2):
--- main.py
+++ main.py
@@ -9,8 +9,9 @@ from get_neighboring_cliques import *
 from get_random_clique import *
 from find_optimal_clique import *
 
-# graph_data = read_graph("Datasets/teste.txt", False)
-g, vertex_dict = read_graph("Datasets/frb30-15-2.clq", False)
+# g, vertex_dict = read_graph("Datasets/frb30-15-2.clq", False)
+g, vertex_dict = read_graph("Datasets/KarateClub.txt", False)
+print("finished reading graph")
 
 nodes = []
 for v in g.vp.labels:
@@ -26,7 +27,9 @@ for v in g.vp.labels:
 #print()
 #print(get_neighboring_cliques(g, vertex_dict, ['0','1','2']))
 initial_clique = get_random_clique(g, vertex_dict)
+print("already got initial clique, now we are searching for optimal")
 find_optimal_clique(g, vertex_dict, initial_clique, 10, 100, 0.9)
 print()
 
+print("bronkerbosch2")
 bron_kerbosch2([], nodes, [], g, vertex_dict)
