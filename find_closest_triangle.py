f3924e0cb50b7b6c1fd7d644c8c05522b2fb3c53
Pamela Tabak <pamelatabak@poli.ufrj.br>
2018-05-27 17:24:34 -0300

    finding all triangles that are at same distance from pivot

--- find_closest_triangle.py
+++ find_closest_triangle.py
@@ -4,13 +4,14 @@ from get_neighbors import *
 
 # we want to find closest triangle from node inside a clique.
 # therefore, this triangle can`t be inside the clique we are checking
-def find_closest_triangle (g, vertex_dict, pivot, clique):
+def find_closest_triangles (g, vertex_dict, pivot, clique):
 	marked_dict = {}
 	queue = []
 	queue.append(pivot)
 	marked_dict[pivot] = True
+	triangles = []
 
-	while (len(queue) != 0):
+	while (len(queue) != 0 and len(triangles) == 0):
 		w = queue.pop(0) #BFS
 		for e in g.edges(): #edge u, v
 			u = e.source()
@@ -20,14 +21,17 @@ def find_closest_triangle (g, vertex_dict, pivot, clique):
 				t.append(g.vp.labels[v])
 				t.append(w)
 				t.append(g.vp.labels[u])
+				t.sort()
 				if (sublist(t, clique)):
 					continue
-				return t
+				if (t not in triangles):
+					triangles.append(t)
 		neighbors = get_neighbors(w, g, vertex_dict)
 		for n in neighbors:
 			if (n not in marked_dict):
 				marked_dict[n] = True
 				queue.append(n)
+	return triangles
 
 def sublist(list1, list2):
 	for element in list1:
--- main.py
+++ main.py
@@ -15,13 +15,13 @@ nodes = []
 for v in g.vp.labels:
 	nodes.append(v)
 
-#print(get_closest_maximal_clique (g, vertex_dict, nodes[0], ['0', '1', '2']))
+# print(get_closest_maximal_clique (g, vertex_dict, nodes[0], ['0', '1', '2']))
 #print()
-#print(get_closest_maximal_clique (g, vertex_dict, nodes[0], ['1','3','9']))
+print(get_closest_maximal_clique (g, vertex_dict, nodes[0], ['1','3','9']))
 #print()
 #print(find_closest_triangle (g, vertex_dict, nodes[0], ['0','1','2']))
 #print()
 #print(find_triangle(g, vertex_dict))
 #print()
 
-BronKerbosch2([], nodes, [], g, vertex_dict)
+# BronKerbosch2([], nodes, [], g, vertex_dict)
