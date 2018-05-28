1d85f5adde0f7cb2fe7ba2b1d18c71ce79b6676c
Pamela Tabak <pamelatabak@poli.ufrj.br>
2018-05-28 19:56:40 -0300

    finding all triangles at min distance from pivot

--- find_closest_triangles.py
+++ find_closest_triangles.py
@@ -11,14 +11,24 @@ def find_closest_triangles (g, vertex_dict, pivot, clique):
 	marked_dict[pivot] = True
 	triangles = []
 
-	while (len(queue) != 0 and len(triangles) == 0):
+	#min distance between pivot and any triangle
+	dist 	 = -1
+	bfs_dist = {} #key is node and value is distance from pivot
+	bfs_dist[pivot] = 0
+
+	while (len(queue) != 0):
 		w = queue.pop(0) #BFS
+		if (dist != -1 and bfs_dist[w] > dist):
+			#it means we have already find triangles at min distance and there are no more
+			#nodes at that min distance to be checked
+			break
 		neighbors = get_neighbors(w, g, vertex_dict)
 		for u in range(0, len(neighbors)):
 			n1 = neighbors[u]
 			if (n1 not in marked_dict):
 				marked_dict[n1] = True
 				queue.append(n1)
+				bfs_dist[n1] = bfs_dist[w] + 1
 			for v in range(u + 1, len(neighbors)):
 				n2 = neighbors[v]
 				if (g.edge(vertex_dict[n1], vertex_dict[n2]) != None):
@@ -29,6 +39,9 @@ def find_closest_triangles (g, vertex_dict, pivot, clique):
 					t.sort()
 					if (sublist(t, clique)):
 						continue
+					if (dist == -1):
+						# it means we found the first triangle
+						dist = bfs_dist[w]
 					if (t not in triangles):
 						triangles.append(t)
 	return triangles
