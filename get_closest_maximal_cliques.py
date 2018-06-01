0c5623a96c9ce2005ddf58acb4a1cb9f7079759e
Pamela Tabak <pamelatabak@poli.ufrj.br>
2018-05-31 22:21:07 -0300

    [bugfix] now we are returning all maximal cliques from a triangle

--- get_closest_maximal_cliques.py
+++ get_closest_maximal_cliques.py
@@ -3,19 +3,16 @@ import numpy as np
 from functools import reduce
 from get_neighbors import *
 from find_closest_triangles import *
+import copy
 
 def get_closest_maximal_cliques (g, vertex_dict, pivot, clique):
 	closest_triangles = find_closest_triangles (g, vertex_dict, pivot, clique)
-	maximal_cliques = []
+	maximal_cliques   = []
 	
 	#then we only need to calculate distance between nodes once
 	min_dist = {}
 
 	for closest_triangle in closest_triangles:
-
-		if (is_triangle_inside_maximal_clique (closest_triangle[0], maximal_cliques)):
-			continue
-
 		#first we check if triangle`s "head" (4th item from tuple) is at distance d from any node of the clique
 		#if param is 0, then we don`t need to check. if 1, then we do need to check
 		#we already know that it`s not distance d from pivot
@@ -39,48 +36,84 @@ def get_closest_maximal_cliques (g, vertex_dict, pivot, clique):
 			continue
 
 		#now we need to get maximal clique with this triangle
-		maximal_clique = closest_triangle[0]
+		triangle = closest_triangle[0]
 
 		#we get the neighbors from each node inside clique and see how many match
 		neighbors = []
-		for n in maximal_clique:
+		for n in triangle:
 			neighbors.append(get_neighbors(n, g, vertex_dict))
 		nodes_in_common = reduce(np.intersect1d,neighbors)
-		for n in maximal_clique:
+		for n in triangle:
 			nodes_in_common = np.delete(nodes_in_common, np.where(nodes_in_common == str(n)))
 
 		if (len(nodes_in_common) == 0):
-			maximal_clique.sort()
-			if (maximal_clique not in maximal_cliques):
-				maximal_cliques.append(maximal_clique)
-			continue
-		if (len(nodes_in_common) == 1):
-			for n in nodes_in_common:
-				maximal_clique.append(n)
-				maximal_clique.sort()
-				if (maximal_clique not in maximal_cliques):
-					maximal_cliques.append(maximal_clique)
+			triangle.sort()
+			if (triangle not in maximal_cliques):
+				maximal_cliques.append(triangle)
 			continue
 
-		for u in range(0, len(nodes_in_common)):
-			u_is_inside_clique = True
-			for v in range(u + 1, len(nodes_in_common)):
-				if (g.edge(vertex_dict[nodes_in_common[u]], vertex_dict[nodes_in_common[v]]) == None):
-					u_is_inside_clique = False
-			if (u_is_inside_clique):
-				maximal_clique.append(g.vp.labels[vertex_dict[nodes_in_common[u]]])
-		if (len(maximal_clique) == 3):
-			#it means that none of the nodes in common are adjacent
-			#since we have nodes in common in the first place, maximal clique is 4
-			#we simply add any of the nodes
-			maximal_clique.append(g.vp.labels[nodes_in_common[np.random.randint(len(nodes_in_common))]])
-		maximal_clique.sort()
-		if (maximal_clique not in maximal_cliques):
-			maximal_cliques.append(maximal_clique)
+		cliques = []
+		# preciso gerar todos os possiveis subconjuntos com nodes_in_common
+		# ver se e valido (todos sao arestas)
+		# ver se e maximal (no final)
+
+		binary_subset  = []
+		one_binary     = ""
+		for i in range(len(nodes_in_common)):
+			binary_subset.append(0)
+			if (i == len(nodes_in_common) - 1):
+				one_binary += "1"
+			else:	
+				one_binary += "0"
+
+		binary_subset[0] = 0
+
+		while (not (all(item == '1' for item in binary_subset))):
+			binary = ''.join(map(str, binary_subset))
+
+			binary = bin_add(binary, one_binary)
+			for b in range(len(binary_subset)):
+				if (b >= len(binary)):
+					binary_subset[b] = 0
+				else:
+					binary_subset[b] = binary[b]
+
+			#check if valid clique. each element with 1 needs to be adjacent
+			nodes_to_check = []
+			for i in range(0, len(binary_subset)):
+				if (binary_subset[i] == '1'):
+					nodes_to_check.append(nodes_in_common[i])
+			valid_clique = True
+			for u in range(0, len(nodes_to_check)):
+				if (not valid_clique):
+					break
+				for v in range(u+1, len(nodes_to_check)):
+					if (g.edge(vertex_dict[nodes_in_common[u]], vertex_dict[nodes_in_common[v]]) == None):
+						valid_clique = False
+						break
+			if (valid_clique):
+				clique = copy.copy(triangle)
+				for n in nodes_to_check:
+					clique.append(n)
+				clique.sort()
+				if (clique not in cliques):
+					cliques.append(clique)
+
+		for c1 in range(0, len(cliques)):
+			for c2 in range(0, len(cliques)):
+			#we need to see if clique is maximal or not
+				if (sublist(cliques[c1], cliques[c2])):
+					continue
+				if (clique not in maximal_cliques):
+					maximal_cliques.append(clique)
+
 	return maximal_cliques
 
-def is_triangle_inside_maximal_clique (triangle, maximal_cliques):
-	for clique in maximal_cliques:
-		if (triangle[0] == clique[0] and triangle[1] == clique[1] and triangle[2] == clique[2]):
-			return True
-	return False
+def bin_add(*args): 
+	return bin(sum(int(x, 2) for x in args))[2:]
+
+def sublist(list1, list2):
+	for element in list1:
+		if (element not in list2):
+			return False
+	return True
